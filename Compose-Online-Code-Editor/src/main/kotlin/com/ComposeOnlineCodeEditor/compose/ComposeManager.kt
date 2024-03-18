2b57d03df30ba4e6fac89ad9957de3fb6b48dcab
MarkSaif <mark.sakhalil.ext@orange.com>
2024-03-18 11:16:36 +0200

    -feat : initialized kotlin target

--- Compose-Online-Code-Editor/src/main/kotlin/com/ComposeOnlineCodeEditor/compose/ComposeManager.kt
+++ Compose-Online-Code-Editor/src/main/kotlin/com/ComposeOnlineCodeEditor/compose/ComposeManager.kt
@@ -1,4 +1,19 @@
 package com.ComposeOnlineCodeEditor.compose
- class ComposeManager {
 
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.runtime.currentComposer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+class ComposeManager {
+     @Composable
+    fun Greeting(name: String = "World") {
+        Text(
+            text = "Hello, $name!",
+            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        )
+    }
 }
--- Compose-Online-Code-Editor/src/main/kotlin/com/ComposeOnlineCodeEditor/plugins/Routing.kt
+++ Compose-Online-Code-Editor/src/main/kotlin/com/ComposeOnlineCodeEditor/plugins/Routing.kt
@@ -1,16 +1,27 @@
 package com.ComposeOnlineCodeEditor.plugins
 
+import com.ComposeOnlineCodeEditor.compose.ComposeManager
+import io.ktor.http.*
+import org.jetbrains.compose.web.renderComposable
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
         get("/tocompose") {
-            call.respondText("Hello Compose!")
+            val composeManager:ComposeManager=ComposeManager()
+            val html = renderComposable(requireNotNull(call.request.queryParameters["name"])) {
+                composeManager.Greeting()
+            }.toHtml()
+
+            call.respondText(html, ContentType.Text.Html)
+        }
         }
     }
-}
+
+
